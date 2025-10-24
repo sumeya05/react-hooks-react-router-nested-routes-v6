5553c10df5f97f6ababaf19fe8a3b95588bbab98
Sumeya Hirsi <sumeyahirsi5@gmail.com>
2025-10-24 07:30:26 +0300

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,12 @@
-import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
+import { Outlet, useOutletContext } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([]);
+  const users = useOutletContext();
 
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
+  const userList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />;
   });
 
   return (
@@ -23,10 +16,11 @@ function Home() {
       </header>
       <main>
         <h1>Home!</h1>
+        <Outlet />
         {userList}
       </main>
     </>
   );
-};
+}
 
 export default Home;
